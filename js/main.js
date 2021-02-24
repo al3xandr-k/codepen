aeb4c034597e2b7ab252cfede314fe569769054b
Alexander Kupsha <red2@mail.ru>
2021-02-24 10:06:57 +0200

    finish code pen

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
-const li = document.createElement('li');
+
 
 let arrList = [];
 
+
 btn.addEventListener('click', () => {
-  let newList = {
-    list: input.value
-  }
-
-  arrList.push(newList);
-  console.log('arrList: ', arrList);
-  displayMessages();
+  const li = document.createElement('li');
+  arrList.push(input.value);
+  li.innerHTML = input.value;
+  ul.append(li);
 });
-
-const displayMessages = () => {
-  let displayMessage = '';
-
-  arrList.forEach(() => {
-    displayMessage += `
-    <li>
-      ${input.value}
-    </li>
-    `
-    li.innerHTML = displayMessage;
-    ul.append(li);
-  })
-}
